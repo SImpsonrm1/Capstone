146bac59a685bd7915d84653c7d0eb7368b2fedc
axsomer <alxsmr99@gmail.com>
2023-09-14 16:08:32 -0400

    preliminary file and dependency setup

---
+++ src/data_collection/download_data.py
@@ -0,0 +1,92 @@
+
+from datasets import load_dataset
+
+ds = load_dataset('google/MusicCaps', split='train')
+print(ds)
+
+
+
+# JUST HELPER METHODS
+
+import subprocess
+import os
+from pathlib import Path
+
+def download_clip(
+    video_identifier,
+    output_filename,
+    start_time,
+    end_time,
+    tmp_dir='/tmp/musiccaps',
+    num_attempts=5,
+    url_base='https://www.youtube.com/watch?v='
+):
+    status = False
+
+    command = f"""
+        yt-dlp --quiet --no-warnings -x --audio-format wav -f bestaudio -o "{output_filename}" --download-sections "*{start_time}-{end_time}" {url_base}{video_identifier}
+""".strip()
+
+    attempts = 0
+    while True:
+        try:
+            output = subprocess.check_output(command, shell=True,
+                                                stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as err:
+            attempts += 1
+            if attempts == num_attempts:
+                return status, err.output
+        else:
+            break
+
+    # Check if the video was successfully saved.
+    status = os.path.exists(output_filename)
+    return status, 'Downloaded'
+
+
+def process(example):
+    outfile_path = str(data_dir / f"{example['ytid']}.wav")
+    status = True
+    if not os.path.exists(outfile_path):
+        status = False
+        status, log = download_clip(
+            example['ytid'],
+            outfile_path,
+            example['start_s'],
+            example['end_s'],
+        )
+
+    example['audio'] = outfile_path
+    example['download_status'] = status
+    return example
+
+
+
+
+
+
+
+from datasets import Audio
+
+samples_to_load = 2       # How many samples to load
+cores = 1                 # How many processes to use for the loading
+sampling_rate = 44100     # Sampling rate for the audio, keep in 44100
+writer_batch_size = 1000  # How many examples to keep in memory per worker. Reduce if OOM.
+data_dir = "./Capstone/data/audio" # Where to save the data
+
+# Just select some samples 
+ds = ds.select(range(samples_to_load))
+
+# Create directory where data will be saved
+data_dir = Path(data_dir)
+data_dir.mkdir(exist_ok=True, parents=True)
+
+ds = ds.map(
+        process,
+        num_proc=cores,
+        writer_batch_size=writer_batch_size,
+        keep_in_memory=False
+    ).cast_column('audio', Audio(sampling_rate=sampling_rate))
+
+
+print('\ndone')
